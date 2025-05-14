0afa7e76eecde054ee3072758fff859d2ea3725a
Gatlat Both <148276251+Gatlat-cs2023@users.noreply.github.com>
2025-05-14 09:56:23 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,20 @@
+-- 1. Total number of bookings made by each user
+SELECT 
+    U.user_id,
+    U.first_name,
+    U.last_name,
+    COUNT(B.booking_id) AS total_bookings
+FROM User U
+LEFT JOIN Booking B ON U.user_id = B.user_id
+GROUP BY U.user_id, U.first_name, U.last_name;
+
+-- 2. Rank properties based on number of bookings using window function
+SELECT 
+    P.property_id,
+    P.name,
+    COUNT(B.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(B.booking_id) DESC) AS booking_rank
+FROM Property P
+LEFT JOIN Booking B ON P.property_id = B.property_id
+GROUP BY P.property_id, P.name;
 
