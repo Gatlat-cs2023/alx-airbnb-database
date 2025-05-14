9a97bb536b1fdf772643009fd8d75f41ab899632
Gatlat Both <148276251+Gatlat-cs2023@users.noreply.github.com>
2025-05-14 09:42:42 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,19 @@
+-- 1. Properties with an average rating greater than 4.0
+SELECT *
+FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- 2. Users who have made more than 3 bookings (correlated subquery)
+SELECT *
+FROM User U
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking B
+    WHERE B.user_id = U.user_id
+) > 3;
 
